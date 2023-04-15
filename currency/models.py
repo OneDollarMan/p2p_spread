b3e7f12f5c31fec3221aa0e30ef1548a9482a1d8
OneDollarMan <vladskorikov@gmail.com>
2023-04-15 10:17:10 +0400

    added basic models

--- currency/models.py
+++ currency/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 
 class Currency(models.Model):
-    ...
+    name = models.CharField(max_length=32)
+    abbr = models.CharField(max_length=4)
+    current_usd_price = models.DecimalField(decimal_places=10, max_digits=19)
+
+    def __str__(self):
+        return self.name
+
+
+class Pair(models.Model):
+    currency1 = models.ForeignKey(Currency, related_name='currency1', on_delete=models.CASCADE, default=0)
+    currency2 = models.ForeignKey(Currency, related_name='currency2', on_delete=models.CASCADE, default=0)
--- p2p_spread/settings.py
+++ p2p_spread/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'currency.apps.CurrencyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
