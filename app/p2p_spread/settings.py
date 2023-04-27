64511bf1cf8306f690a616831c1459bcea367992
OneDollarMan <vladskorikov@gmail.com>
2023-04-27 20:55:35 +0400

    prod settings

--- app/p2p_spread/settings.py
+++ app/p2p_spread/settings.py
@@ -28,6 +28,11 @@ ALLOWED_HOSTS = [
     '.localhost', '127.0.0.1', '[::1]', 'coindripper.ru', 'coindripper.site'
 ]
 
+CSRF_TRUSTED_ORIGINS = ["https://coindripper.ru", "https://coindripper.site"]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+
 # Application definition
 
 INSTALLED_APPS = [
