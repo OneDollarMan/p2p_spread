dd80dbfac1d2a0fb0ee0d992d271318227f3b2aa
OneDollarMan <vladskorikov@gmail.com>
2023-07-02 16:37:39 +0400

    postgres

--- app/p2p_spread/settings.py
+++ app/p2p_spread/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'p2p_spread.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 
