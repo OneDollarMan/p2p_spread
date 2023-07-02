0adae5dc44c3fd2d0bfe1f25ddc9b7d92cb2a2f7
OneDollarMan <vladskorikov@gmail.com>
2023-07-02 21:35:50 +0400

    debug fix

--- app/p2p_spread/settings.py
+++ app/p2p_spread/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%1oxk8$o9zqd6d4^8^8v&$xu)b$0&v+m01f7@jssa5rlje1e*8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.localhost', '127.0.0.1', '[::1]', 'coindripper.ru', 'coindripper.site'
